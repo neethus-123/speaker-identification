98364942aac45fcdff34993829a66119ed88a823
neethus-123 <92640544+neethus-123@users.noreply.github.com>
2021-10-16 20:38:00 +0200

    Update speaker-recognition-app.py

--- speaker-recognition-app.py
+++ speaker-recognition-app.py
@@ -70,7 +70,7 @@ import streamlit as st
 #import librosa
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import matplotlib 
 import os
 from PIL import Image
