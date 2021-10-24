d7eb26957487ddcd3f6f0531d36f6ec57ed5f473
neethus-123 <92640544+neethus-123@users.noreply.github.com>
2021-10-24 11:51:02 +0200

    Update speaker-recognition-app.py

--- speaker-recognition-app.py
+++ speaker-recognition-app.py
@@ -101,7 +101,7 @@ file = open('dataset.csv', 'w', newline='')
 with file:
     writer = csv.writer(file)
     writer.writerow(header)
-    path=os.path.dirname(__audio__)
+    path=os.path.dirname(__file__)
 audio = ' neetha  '.split()
 t=audio[0]
 for t in audio:
